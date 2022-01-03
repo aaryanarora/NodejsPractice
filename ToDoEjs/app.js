c79ab69181e25601ddaa0440f2b81f7e65b56026
Aaryan <Aaryanarora1871@gmail.com>
2022-01-03 20:05:29 +0530

    new

--- ToDoEjs/app.js
+++ ToDoEjs/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+var items = ["But food","Eat food"];
+var workItem = [];
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
 app.get("/", function(req, res){
 
     var today = new Date();
@@ -16,8 +19,40 @@ app.get("/", function(req, res){
     };
 
     var day = today.toLocaleDateString("en-US",options);
-    res.render("list",{Typeday: day});
+    res.render("list",{listTitle: day, newListItems: items});
 
+});
+app.post("/",function(req,res){
+
+    let item = req.body.newItem;
+    console.log(req.body.list);
+    if(req.body.list === "Work"){
+        workItem.push(item);
+        res.redirect("/work")
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+})
+
+app.get("/work", function(req,res){
+
+    res.render("list",{listTitle: "Work list",newListItems: workItem})
+});
+
+app.post("/work",function(req,res){
+
+    var item = req.body.newItem;
+    
+    workItem.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about",function(req,res){
+
+    res.render("about");
 });
 
 app.listen(3000,function(){
