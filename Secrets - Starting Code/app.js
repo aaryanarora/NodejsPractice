72d74073bd6f6e627db84f33b4c8732e6e46d014
Aaryan <Aaryanarora1871@gmail.com>
2022-01-09 22:49:15 +0530

    Secret submit

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -33,7 +33,8 @@ const userSchema = new mongoose.Schema({
 
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -88,19 +89,50 @@ app.get("/login",function(req,res){
 }) 
 app.get("/secrets",function(req,res){
 
-    if(req.isAuthenticated())
-    {
-        res.render("secrets");
-    }
-    else{
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUsers){
+
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    })
 });
 app.get("/register",function(req,res){
 
     res.render("register");
 }) 
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }
+    else{
+        res.redirect("/login");
+    }
+})
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
 
+    User.findById(req.user.id, function(err, foundUser){
+
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+})
 app.get("/logout", function(req, res){
 
     req.logout();
